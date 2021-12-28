a918212485d3fea553e389fe5aca5e1b4d6a804c
Rian Perassoli <rian@torfresma.com.br>
2021-12-28 15:10:07 -0300

    Adicionado icone e tema

--- backup_maker.dpr
+++ backup_maker.dpr
@@ -2,13 +2,17 @@ program backup_maker;
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {Form1};
+  Main in 'Main.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.Title := 'Backup Maker';
+  TStyleManager.TrySetStyle('Metropolis UI Blue');
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
